d8a7d6958f26ffb95477017d0f63856c329cb597
websonly <websonly1010@gmail.com>
2025-08-03 15:18:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Bienvenido a Testimonial Builder</h1>
+    </main>
+  );
+}
 
